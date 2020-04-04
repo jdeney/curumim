a243ef816de99f9d9e4bd100216f2304cfa3c871
Deney Araujo <deneyaraujo@gmail.com>
2020-04-04 18:26:01 -0300

    alterando -> Add novos nomes de exemplo

--- backupgit.sh
+++ backupgit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/deney/Dropbox/doutorado/Bioinfo/projetos_paralelos/curumim
-HOME=/home/deney git push origin master
+#cd /home/deney/Dropbox/doutorado/Bioinfo/projetos_paralelos/curumim
+#HOME=/home/deney git push origin master
+
+#HOME=/home/deney ssh -Tvvv github.com
